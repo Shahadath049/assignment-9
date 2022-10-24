3944a88451818eebabe25d1fe06ab997e0b097f1
Shahadath049 <zamirshahadath049@gamail.com>
2022-10-24 16:49:25 +0600

    header area added

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,26 +8,29 @@ const Home = () => {
 
 
     return (
-        <div>
+        <div className=''>
             <div className=' h-100 '  style={{
                 backgroundImage: "url(" + "https://img.freepik.com/free-vector/network-mesh-wire-digital-technology-background_1017-27428.jpg?w=1380&t=st=1666564924~exp=1666565524~hmac=de21c6390042296ce985e3100a9d29921f381747aed7a58b9871cd4f1ed3134a" + ")",
                 backgroundPosition: 'center',
                 backgroundSize: 'cover',
                 backgroundRepeat: 'no-repeat'
             }} >
-               <div className='p-40'>
-               <h3 className='mb-10 text-slate-700 font-bold text-5xl'>Learn Web Programming easily...</h3>
+               <div className='p-24'>
+               <h3 className='mb-10 pr-10 text-slate-700 font-bold text-4xl'>Learn Web Programming easily...</h3>
                 <p className='pb-30 text-slate-800 font-semibold text-xl'>Start your learn-and-earn journey with Meta.Meta teaches you how to write smart contracts. There is a global shortage of Web3 coders. Get paid to learn how to code on blockchain. World-Class Education. Decentralized Application. Experienced Mentors.</p>
                </div>
             </div>
-
-            {
+            <div className='mx-20 mt-10 grid lg:grid-cols-4 md:grid-cols-2 gap-4 justify-center'>
+                {
                 topics.data.map(topic =><Topic
                     key={topic.id}
                     topic={topic}
 
                 ></Topic>)
             }
+            </div>
+
+            
 
         </div >
     );
--- src/components/Topic.js
+++ src/components/Topic.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Topic = ({topic}) => {
-    const {name,logo,total}= topic;
+const Topic = ({ topic }) => {
+    const { name, logo } = topic;
     return (
         <div>
-                there are {name}{total} topics.           
+
+            <div className=" max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-600 dark:border-gray-700">
+               
+                    <img className="p-10 rounded-t-lg" src={logo} alt=""/>
+                
+                <div className=" flex justify-between sm:ju p-5">
+                    
+                        <h5 className="mb-2 mr-2 text-xl font-semi bold tracking-tight text-gray-900 dark:text-white">{name}</h5>
+                   
+                
+                    <Link  className="inline-flex items-center py-1 px-2 text-sm font-medium text-center text-white bg-blue-700 rounded-md hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        Start Practice
+                        <svg aria-hidden="true" className="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" ></path></svg>
+                    </Link>
+                </div>
+            </div>
+
+
         </div>
     );
 };
